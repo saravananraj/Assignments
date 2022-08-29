06d7dee0f916f0fe472f09b3e80763a638a887b8
Saravanan <saravananapec@gmail.com>
2022-08-29 17:17:11 +0530

    Added the Eureka client in dependency

--- PriceService/src/main/java/com/saravanan/springcloud/PriceServiceApplication.java
+++ PriceService/src/main/java/com/saravanan/springcloud/PriceServiceApplication.java
@@ -2,8 +2,10 @@ package com.saravanan.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PriceServiceApplication {
 
 	public static void main(String[] args) {
